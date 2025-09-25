021edafffd891e7a10bd48fda95075f13c295de2
bangadjie <masadjie927@gmail.com>
2025-09-25 18:09:56 +0700

    Side1, Front update

---
+++ Side.java
@@ -0,0 +1,34 @@
+public class Side {
+
+    
+}
+
+// public class Side {
+//     private String deskripsi;
+//     private boolean isSelesai;
+
+//     public Side(String deskripsi) {
+//         this.deskripsi = deskripsi;
+//         this.isSelesai = false; // Tugas baru otomatis belum selesai
+//     }
+
+//     // Getter untuk deskripsi
+//     public String getDeskripsi() {
+//         return deskripsi;
+//     }
+
+//     // Setter untuk deskripsi
+//     public void setDeskripsi(String deskripsi) {
+//         this.deskripsi = deskripsi;
+//     }
+
+//     // Getter untuk status selesai
+//     public boolean isSelesai() {
+//         return isSelesai;
+//     }
+
+//     // Setter untuk status selesai
+//     public void setSelesai(boolean isSelesai) {
+//         this.isSelesai = isSelesai;
+//     }
+// }
